307bada7fc0daf97ec58dd82335d7bbbb9a8c89d
Andrés Fabián Ramos <andresram@unisabana.edu.co>
2020-01-26 13:11:16 -0500

    Added currency format new

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 final dateFormat = new DateFormat('dd-MM-yyyy');
+//final currencyFormat = new NumberFormat.currency(locale: );
 
 final primaryColor = Color(0xff696b9e);
 final secondaryColor = Color(0xfff29a94);
